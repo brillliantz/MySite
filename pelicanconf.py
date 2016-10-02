3bf20d8872d9588f7ae1118a5995b9c6c552bb2c
William <brillliantz@outlook.com>
2016-10-02 18:39:37 +0800

    Elegant theme, new features

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,7 +11,7 @@ PATH = 'content'
 TIMEZONE = 'Asia/Shanghai'
 DEFAULT_DATE_FORMAT = '%b.%d, %Y (%Z)'
 
-THEME = './theme/xycoding-gum/'
+THEME = './theme/elegant-1.3/'
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
@@ -36,12 +36,38 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
+# use folder name as category
+USE_FOLDER_AS_CATEGORY = True
+# use filename as slug
+FILENAME_METADATA = '(?P<slug>.*)'
+
+#----------------------------------------------------------------------
+# show and store directories setting:
+ARTICLE_URL = '{category}/{slug}.html'
+ARTICLE_SAVE_AS = ARTICLE_URL
+
+PAGE_URL = 'page_{slug}.html'
+PAGE_SAVE_AS = PAGE_URL
+
+# category index ("articles in xxx category" page)
+CATEGORY_URL = '_categories/{slug}_index.html'
+# CATEGORY_URL = 'category_{slug}/index.html'
+CATEGORY_SAVE_AS = CATEGORY_URL
+
+TAG_URL = '_tags/{slug}_index.html'
+TAG_SAVE_AS = TAG_URL
+TAGS_SAVE_AS = 'tags.html'
+
+AUTHOR_URL = '_authors/{slug}_index.html'
+AUTHOR_SAVE_AS = AUTHOR_URL
+#----------------------------------------------------------------------
+
 #---------------------------------------------------------------------
 # ipynb plugin
 MARKUP = ('md', 'ipynb')
 
 PLUGIN_PATHS = ['./plugins/',]
-PLUGINS = ['ipynb.markup', 'render_math', 'sitemap', 'random_article', 'tag_cloud',]
+PLUGINS = ['ipynb.markup', 'render_math', 'sitemap','random_article', 'tag_cloud', 'extract_toc', 'tipue_search']
 # sitemap plugin options:
 SITEMAP = {
     'format': 'xml',
@@ -65,3 +91,29 @@ GITHUB_URL = 'https://github.com/brillliantz'
 TWITTER_URL = 'http://twitter.com'
 FACEBOOK_URL = 'http://www.facebook.com'
 LINKEDIN_URL = 'https://www.linkedin.com/in/bingnliu/en'
+
+#----------------------------------------------------------------------
+# Below are settings only for the Elegent theme
+
+# About Me section on homepage
+LANDING_PAGE_ABOUT = {'title': "I'm a junior in physics, focusing on quant trading.",
+                      'details': "Always seeking the infinite beauty of the world in my finite existence."}
+
+# PROJECTS section on homepage
+PROJECTS = [
+    {'name': 'Citi Financial Innovation Application Competation',
+     'url': 'http://www.citigroup.com/china/csts/EducationProgram/AboutEducation_CFC.html',
+     'description': 'Leader. Lead the team to design a robo-advisor platform, allocating among major assets.',},
+    {'name': 'Quantitative Analyst Intern @Wizardquant',
+     'url': 'http://www.wizardquant.com/en/html/aboutus.html',
+     'description': 'Research on statistical modeling.',}
+]
+
+
+# Here are the variables that you should set in your configuration to get the most out of Elegant
+MD_EXTENSIONS = ['codehilite(css_class=highlight)', 'extra', 'headerid', 'toc']
+DIRECT_TEMPLATES = (('index', 'tags', 'categories','archives', 'search', '404'))
+#STATIC_PATHS = ['theme/images', 'images']
+
+#----------------------------------------------------------------------
+
